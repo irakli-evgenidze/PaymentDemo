08463569c01f68125825c573343ac4381cd4f7e9
Irakli Evgenidze <iraklievgenidze17@gmail.com>
2026-04-08 20:24:44 +0400

    Add payment service for creating and processing payments

---
+++ src/PaymentService.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class PaymentService {
+    private List<Payment> paymentHistory = new ArrayList<>();
+
+    public Payment createPayment(double amount, PaymentMethod method) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Amount must be greater than 0");
+
+        }
+
+
+        Payment payment = new Payment(UUID.randomUUID().toString(), amount, method);
+        paymentHistory.add(payment);
+        return payment;
+    }
+    public void proccessPayment(Payment payment) {
+        if (payment.getAmount() > 1000) {
+            payment.setStatus(PaymentStatus.FAILED);
+        }else {
+            payment.setStatus(PaymentStatus.SUCCESS);
+        }
+    }
+    public List<Payment> getPaymentHistory() {
+        return paymentHistory;
+    }
+}
