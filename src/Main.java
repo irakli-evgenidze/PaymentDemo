89d427203eabde1af93792aab54418d019d41df8
Irakli Evgenidze <iraklievgenidze17@gmail.com>
2026-04-08 20:35:47 +0400

    Wire payment service into main flow and print payment history

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("payment Processor App Started");
+        PaymentService paymentService = new PaymentService();
+
+        Payment payment1 = paymentService.createPayment(250.50, PaymentMethod.CARD);
+        paymentService.proccessPayment(payment1);
+
+        Payment payment2 = paymentService.createPayment(1500, PaymentMethod.TRANSFER);
+        paymentService.proccessPayment(payment2);
+
+        for (Payment payment : paymentService.getPaymentHistory()) {
+            System.out.println(payment);
+        }
     }
 }
