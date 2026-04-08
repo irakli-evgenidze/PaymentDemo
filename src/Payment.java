323ca9b293b1f6ff42fd5a9c22a340a6eb74493b
Irakli Evgenidze <iraklievgenidze17@gmail.com>
2026-04-08 20:05:00 +0400

    Add Payment model with fields and toString

---
+++ src/Payment.java
@@ -0,0 +1,42 @@
+public class Payment {
+    private String id;
+    private double amount;
+    private PaymentMethod method;
+    private PaymentStatus status;
+
+    public Payment(String id, double amount, PaymentMethod method) {
+        this.id = id;
+        this.amount = amount;
+        this.method = method;
+        this.status = PaymentStatus.PENDING;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public PaymentMethod getMethod() {
+        return method;
+    }
+
+    public PaymentStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(PaymentStatus status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "payment{" + "id=' " +id +
+                '\'' + ", ammount=" +
+                amount + ", method=" + method +
+                ", status=" + status + '}';
+    }
+}
