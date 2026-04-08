8c1bfe297e8b3c8c8770c933c547b17c162bc0f7
Irakli Evgenidze <iraklievgenidze17@gmail.com>
2026-04-08 20:45:46 +0400

    Add exeption handling for invalid payment amounts

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         PaymentService paymentService = new PaymentService();
-
+    try {
         Payment payment1 = paymentService.createPayment(250.50, PaymentMethod.CARD);
         paymentService.proccessPayment(payment1);
 
         Payment payment2 = paymentService.createPayment(1500, PaymentMethod.TRANSFER);
         paymentService.proccessPayment(payment2);
+
+        Payment payment3 = paymentService.createPayment(-10, PaymentMethod.CASH);
+        paymentService.proccessPayment(payment3);
+    }catch (IllegalArgumentException e) {
+        System.out.println("Error:" + e.getMessage());
+    }
 
         for (Payment payment : paymentService.getPaymentHistory()) {
             System.out.println(payment);
