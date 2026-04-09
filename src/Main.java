75483b4a3360e21c5f615d40f506e56ea9cc198b
Irakli Evgenidze <iraklievgenidze17@gmail.com>
2026-04-09 23:27:22 +0400

    Add interacitive menu for payment operations

--- src/Main.java
+++ src/Main.java
@@ -5,31 +5,80 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         PaymentService paymentService = new PaymentService();
 
-        try {
-            System.out.println("Enter payment amount: ");
-            double amount = scanner.nextDouble();
+        while (true){
+            System.out.println("/n=== Payment System ===");
+            System.out.println("1. Create Payment");
+            System.out.println("2. View All Payments");
+            System.out.println("3. Find Payment");
+            System.out.println("4. Refund Payment");
+            System.out.println("5. Exit");
 
+            System.out.println("Choose option: ");
+            int choice = scanner.nextInt();
 
-            System.out.println("Choose payment method: 1 = CARD, 2 = CASH, 3 = TRANSFER");
-            int methodInput = scanner.nextInt();
+            try {
+                if (choice ==1) {
+                    System.out.println("Enter amount: ");
+                    double amount = scanner.nextDouble();
 
-            PaymentMethod method;
+                    System.out.println("Choose methood: 1 = CARD, 2 = CASH, 3 = TRANSFER");
+                    int methodInput = scanner.nextInt();
 
-            if (methodInput == 1) {
-                method = PaymentMethod.CARD;
-            } else if (methodInput == 2) {
-                method = PaymentMethod.CASH;
-            }else {
-                method = PaymentMethod.TRANSFER;
-            }
-            Payment payment = paymentService.createPayment(amount, method);
-            paymentService.processPayment(payment);
+                    PaymentMethod method;
+                    if (methodInput == 1) {
+                        method = PaymentMethod.CARD;
+                    } else if (methodInput == 2) {
+                        method = PaymentMethod.CASH;
+                    } else {
+                        method = PaymentMethod.TRANSFER;
+                    }
 
-            } catch (IllegalArgumentException e) {
+                    Payment payment = paymentService.createPayment(amount, method);
+                    paymentService.processPayment(payment);
+
+                    System.out.println("Created: " + payment);
+                } else if (choice == 2) {
+                    for (Payment p : paymentService.getPaymentHistory()) {
+                        System.out.println(p);
+                    }
+
+                } else if (choice == 3) {
+                    System.out.println("Enter payment ID: ");
+                    String id = scanner.next();
+
+                    Payment found = paymentService.findPaymentById(id);
+
+                    if (found != null) {
+                        System.out.println(found);
+                    } else {
+                        System.out.println("Not found");
+                    }
+
+                } else if (choice == 4) {
+                    System.out.println("Enter payment ID to refund");
+                    String id = scanner.next();
+
+                    Payment payment = paymentService.findPaymentById(id);
+
+                    if (payment != null) {
+                        paymentService.refundPayment(payment);
+                        System.out.println("Refunded: " + payment);
+
+                    } else {
+                        System.out.println("Payment not found");
+                    }
+
+                } else if (choice == 5) {
+                    System.out.println("Exiting...");
+                    break;
+
+                }
+            } catch (Exception e) {
                 System.out.println("Error: " + e.getMessage());
-
+            }
         }
         scanner.close();
+
 
 
     try {
